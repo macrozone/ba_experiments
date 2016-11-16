df30d4d5453ad6d6a9fbb3f57360bc329c42c5bf
Marco <macrozone@gmail.com>
2016-11-16 14:16:48 +0100

    add home nav

--- app/client/modules/core/components/home.jsx
+++ app/client/modules/core/components/home.jsx
@@ -1,22 +1,38 @@
 import React from 'react';
+import withTheme from '/manul-utils/with_theme';
 
-import {Layer, Rect, Stage, Group} from 'react-konva';
-const MyRect = () => (
-  <Rect
-      x={10} y={10} width={50} height={50}
-      fill={"red"}
-      shadowBlur={10}
-      onClick={() => alert('click')}
-  />
-);
-const Home = () => (
-  <div>
-    <Stage width={700} height={700}>
-       <Layer>
-           <MyRect/>
-       </Layer>
-     </Stage>
-  </div>
-);
+import {Button} from 'react-bootstrap';
 
+
+const Styles = ({style, ...props}, theme) => {
+  return {
+    base: [
+      {
+        textAlign: 'center',
+        marginTop: 60
+      },
+      style // allow override
+    ]
+  };
+};
+
+const Component = ({styles}) => {
+  return (
+    <div style={styles.base}>
+      <Button href="/pet3dviewer">PET 3dViewer</Button>
+      <Button href="/annotation2d">2D Annotation Sample App</Button>
+    </div>
+  );
+};
+
+const Home = withTheme(Styles, Component);
+
+Home.propTypes = {
+};
+
+Home.defaultProps = {
+};
+
+Home.displayName = 'Home';
+Component.displayName = 'withTheme(Home)';
 export default Home;

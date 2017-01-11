b877c09e1b11855c5b7d8a7b6a6b0a977b9be164
Marco <macrozone@gmail.com>
2017-01-11 15:10:13 +0100

    comments

--- app/client/modules/threed_viewer/libs/get_cursor_position_on_ray.js
+++ app/client/modules/threed_viewer/libs/get_cursor_position_on_ray.js
@@ -1,6 +1,6 @@
 import THREE from 'three';
 
-export default ({camera, markerRay, cameraRay}) => {
+export default ({ camera, markerRay, cameraRay }) => {
   // we want to show a marker on the line where the mouse-cursor is.
   // The user does not need to point directly on the line,
   // instead the mouse cursor can be below or above the line and we want to show
@@ -24,13 +24,16 @@ export default ({camera, markerRay, cameraRay}) => {
   // n needs to be normal to this ray as well, but n needs also to be normal to camera-up.
   // this can be calculate with the cross-product of the ray-direction with camera-up:
 
-  const up = new THREE.Vector3(0,1,0);
+  const up = new THREE.Vector3(0, 1, 0);
+  // up would be enough, but if the camera is exactly on top of the model, it will break
+  // that's why we take the cameras rotation into account
   const cameraUp = up.applyQuaternion(new THREE.Quaternion(
     camera.quaternion._x, camera.quaternion._y, camera.quaternion._z, camera.quaternion._w
   ));
+
   const n = cameraUp.cross(cameraRay.direction);
 
-  // we need to norm it:
+  // we need to norm and orient it:
   const dotProduct = p.dot(n);
 
   const n0 = dotProduct > 0 ? n.normalize() : n.normalize().negate();
@@ -42,5 +45,4 @@ export default ({camera, markerRay, cameraRay}) => {
   const plane = new THREE.Plane(n0, d);
   // and the intersection
   return new THREE.Ray().copy(markerRay).intersectPlane(plane);
-
 };
